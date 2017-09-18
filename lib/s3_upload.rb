1a82909fa630764b88b6db11b33f60212a66b923
matsuhisa <matsuhisa+github@gmail.com>
2017-09-18 19:53:58 +0900

    WIP

---
+++ lib/s3_upload.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+require 'aws-sdk'
+require "pry"
+
+BUCKET_NAME = "rails-fileup-matsuhisa"
+DIRECTORY_PATH = "foo/bar"
+
+class S3Upload
+  def initialize
+    @s3 = Aws::S3::Resource.new
+  end
+
+  def upload_file_path(file_path)
+    upload_file_path = "#{DIRECTORY_PATH}/#{file_name(file_path)}"
+    bucket = @s3.bucket(BUCKET_NAME)
+    bucket.object(upload_file_path).upload_file(file_path, { acl: "public-read"} )
+  end
+
+  private
+
+  def file_name(file_path)
+    extname = File.extname file_path
+    "#{Time.now.to_i}_foo#{extname}"
+  end
+
+end
---
+++ s3.rb
@@ -0,0 +1,3 @@
+require './lib/s3_upload.rb'
+
+S3Upload.new.upload_file_path("./Octocat.png")
